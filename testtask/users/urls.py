720e54d19dfb6031b43ffd70b17d8b7c3bb33126
Popov Aleksandr <paracosm17@yandex.ru>
2023-03-24 15:36:23 +1000

    Create urls.py

---
+++ testtask/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = "users"
+
+urlpatterns = [
+    path("register/", views.UserRegisterationAPIView.as_view(), name="create-user"),
+    path("login/", views.UserLoginAPIView.as_view(), name="login-user"),
+    path("logout/", views.UserLogoutAPIView.as_view(), name="logout-user"),
+    path("user/", views.UserAPIView.as_view(), name="user-info"),
+    path("user/settings/", views.CustomUserUpdate.as_view(), name="user-update"),
+    path("cars/", views.CarApi.as_view(), name="cars"),
+]
