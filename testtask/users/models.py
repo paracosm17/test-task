cd1e787ac93a58453bca57807ede25be543c910a
Popov Aleksandr <paracosm17@yandex.ru>
2023-03-24 15:37:54 +1000

    Add first_name, last_name, country

--- testtask/users/models.py
+++ testtask/users/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
-class Car(models.Model):
-    make = models.CharField(max_length=50)
-    model = models.CharField(max_length=50)
-    year = models.PositiveIntegerField()
-    color = models.CharField(max_length=50)
-    owners = models.ManyToManyField('CustomUser', through='Ownership')
-
-    def __str__(self):
-        return f"{self.make} {self.model} {self.year}"
-
-    class Meta:
-        verbose_name = "Car"
-        verbose_name_plural = "Cars"
-
-
 class CustomUser(AbstractUser):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     email = models.EmailField(db_index=True, unique=True)
@@ -24,6 +9,10 @@ class CustomUser(AbstractUser):
     is_staff = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    first_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
@@ -36,9 +25,16 @@ class CustomUser(AbstractUser):
         verbose_name_plural = "Users"
 
 
-class Ownership(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Владелец')
+
+    def __str__(self):
+        return f"{self.make} {self.model} {self.year}"
 
     class Meta:
-        unique_together = ('user', 'car')
+        verbose_name = "Car"
+        verbose_name_plural = "Cars"
