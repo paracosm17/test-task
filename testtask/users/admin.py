c00655d4759cf33f6b21f55a0e683a6a16a31cd4
Popov Aleksandr <paracosm17@yandex.ru>
2023-03-24 15:37:08 +1000

    Add admin panel

--- testtask/users/admin.py
+++ testtask/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, Car
+
+
+class CarAdminInline(admin.TabularInline):
+    model = Car
+
+
+class CustomUserAdmin(admin.ModelAdmin):
+    model = CustomUser
+    inlines = (CarAdminInline, )
+
+
+class CarAdmin(admin.ModelAdmin):
+    model = Car
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Car, CarAdmin)
