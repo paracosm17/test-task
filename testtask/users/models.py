35c78d3244a5dbf9366a09b3327a93169d8170e5
Popov Aleksandr <paracosm17@yandex.ru>
2023-03-24 10:09:36 +1000

    Create CustomUser and Car models

--- testtask/users/models.py
+++ testtask/users/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    color = models.CharField(max_length=50)
+    owners = models.ManyToManyField('CustomUser', through='Ownership')
+
+
+class CustomUser(AbstractUser):
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
+    class Meta:
+        related_name = "User"
+        related_name_plural = "Users"
+
+
+class Ownership(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'car')
