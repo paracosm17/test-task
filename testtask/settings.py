8c1d4bf9a57f3e542465383e82701a2fd16c345f
Popov Aleksandr <paracosm17@yandex.ru>
2023-03-27 18:10:42 +1000

    python dot-env

--- testtask/settings.py
+++ testtask/settings.py
@@ -13,23 +13,24 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
-import environ
+import dotenv
 
-env = environ.Env()
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG'].lower() == 'true'
+DEBUG = os.environ['SECRET_KEY'].lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
